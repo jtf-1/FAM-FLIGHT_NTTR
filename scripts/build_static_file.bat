4b6330b6e939d5ea20720c86deda1bc02a9ba58e
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2022-04-13 21:01:03 +0100

    Add Closed Beta Versions

--- scripts/build_static_file.bat
+++ scripts/build_static_file.bat
@@ -13,7 +13,7 @@ SET parent=%~dp0
 SET log=%parent%logs\%me%.log
 
 :: name of static mission file to create
-SET build_filename=FAM-FLIGHT_NTTR.lua
+SET build_filename=NTTR_NOMODS.lua
 ECHO Static file name:           %build_filename%
 
 SET build_path=%parent%static\
@@ -37,6 +37,7 @@ ECHO.
 :: Add dynamic files
 CALL :buildfile mission_init.lua
 CALL :buildfile devcheck.lua
+CALL :buildfile msrs.lua
 CALL :buildfile adminmenu.lua
 CALL :buildfile missiontimer.lua
 CALL :buildfile missiletrainer.lua
--- scripts/dynamic/mission_init.lua
+++ scripts/dynamic/mission_init.lua
@@ -1,4 +1,4 @@
-env.info( '*** JTF-1 STATIC MISSION SCRIPT START ***' )
+env.info( '[JTF-1] *** JTF-1 STATIC MISSION SCRIPT START ***' )
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- BEGIN INIT
@@ -10,26 +10,8 @@ _SETTINGS:SetPlayerMenuOff()
 --- debug on/off
 BASE:TraceOnOff(false) 
 
-JTF = {}
---- activate admin menu option in admin slots if true
-JtfAdmin = true 
-
--- mission flag for triggering reload/loading of missions
-flagLoadMission = 9999
-
--- value for triggering loading of base mission
-flagBaseMissionValue = 1
-
--- value for triggering loading of dev mission
-flagDevMissionValue = 99
-
---- Name of client unit used for admin control
-adminUnitName = "XX_" -- string to locate within unit name for admin slots
-
---- Dynamic list of all clients
---JTF.SetClient = SET_CLIENT:New():FilterStart()
-
--- flag value to trigger reloading of DEV mission
-devMission = 99
-
+JTF1 = {
+    missionRestart = "ADMIN9999", -- Message to trigger mission restart via jtf1-hooks
+    flagLoadMission = 9999, -- flag for load misison trigger
+}
 --- END INIT
